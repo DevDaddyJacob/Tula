65ea69e43577419dc0c770572971a152c7ca14d8
Jacob Paulin <jacobwpaulin@gmail.com>
2025-03-31 13:43:13 -0400

    chore(java): Added base for java work

---
+++ tula-java/src/com/devjacob/tula/Tula.java
@@ -0,0 +1,9 @@
+package com.devjacob.tula;
+
+import java.io.IOException;
+
+public class Tula {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello Tula!");
+    }
+}
